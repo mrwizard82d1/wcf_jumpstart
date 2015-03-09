f76cac0319f93dc647d86c9b6413e4794175e1b6
Larry Jones <mrwizard82d1@gmail.com>
2015-03-09 11:01:52 -0500

    Implemented service with behaviors.

---
+++ ZzaApp/ZzaServices/ZzaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Zza.Data;
+using Zza.Entities;
+
+namespace ZzaServices
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    class ZzaService : IZzaServices, IDisposable
+    {
+        readonly ZzaDbContext _context = new ZzaDbContext();
+
+        public List<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // Scopes entire call to occur within a (platform-created) 
+        // transaction.
+        [OperationBehavior(TransactionScopeRequired = true)]
+        public void SubmitOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            order.OrderItems.ForEach(oi => _context.OrderItems.Add(oi));
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
