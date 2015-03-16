a6662d668b74b2a4b74aed25717199f7fd1f4178
Larry Jones <mrwizard82d1@gmail.com>
2015-03-15 21:06:11 -0500

    Began trying to test client without UI. Failed.
    I made some progress; however, when I ran the unit tests, the unit test
    code reported that it could not find the default endpoint. I suspect
    this error results from the app.config file being defined in the
    Zza.Client project instead of the Zza.Client.Tests.Integration project.
    
    I chose not to pursue this further so that I could finish the videos.

---
+++ ZzaApp/Zza.Client.Tests.Integration/MainWindowVMTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace Zza.Client.Tests.Integration
+{
+    [TestFixture]
+    public class MainWindowVmTests
+    {
+        [TestCase]
+        public void Ctor_ServiceRunning_AllProducts()
+        {
+            var cut = new MainWindowVm();
+
+            var allProductsCount = cut.Products.Count;
+
+            Assert.That(15, Is.EqualTo(allProductsCount));
+        }
+    }
+}
---
+++ ZzaApp/Zza.Client/MainWindowVm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Zza.Entities;
+
+namespace Zza.Client
+{
+    class MainWindowVm
+    {
+        public MainWindowVm()
+        {
+            LoadProductsAndCustomers();
+        }
+
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public ObservableCollection<Product> Products { get; set; }
+
+        private void LoadProductsAndCustomers()
+        {
+            var proxy = new ZzaService.ZzaServicesClient("BasicHttpBinding_IZzaServices");
+            Products = proxy.GetProducts();
+            Customers = proxy.GetCustomers();
+        }
+
+    }
+}
